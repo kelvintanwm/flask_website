065ed89b85bb9d98074662f02c302bc27e41ea7f
Kelvintanwm <kelvintanwm96@gmail.com>
2023-07-19 18:26:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ app = create_app()
 
 # Only if we run this file and not if we import, then we will execute the line below
 if __name__ == '__main__':
+    # debug = True will indicate that everytime the code is changed, the webserver will be refreshed
     app.run(debug = True)
