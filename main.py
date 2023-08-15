b35bbb2a4547b3bf321d40ce0e9430350a4a764f
Kelvintanwm <kelvintanwm96@gmail.com>
2023-08-15 15:29:42 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from website import create_app
 
 app = create_app()
 
-# Only if we run this file and not if we import, then we will execute the line below
+# Only if we run this file and not importing the file, then we will execute the line below
+# Only run the web server if we run the file, not importing
 if __name__ == '__main__':
     # debug = True will indicate that everytime the code is changed, the webserver will be refreshed
+    # Turn this off during production
     app.run(debug = True)
