67fec40d09f60c9d0e928f78bdb34a0534d801f0
KTWM <62528038+kelvintanwm@users.noreply.github.com>
2023-09-11 13:14:21 +0800

    Cleaning up

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user
 views = Blueprint('views',__name__)
 
 @views.route('/')
-@login_required
+# @login_required
 def home():
     """
     Home page of the website
